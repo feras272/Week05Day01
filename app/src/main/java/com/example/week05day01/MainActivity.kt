e10c0a43ede281ff0e95ae7665da841d59bcec0b
feras alnowiser <ferasdev@gmail.com>
2022-07-08 16:34:20 +0300

    - adding view and connected by its id in MainActivity - adding cancel icon in drawable file

--- app/src/main/java/com/example/week05day01/MainActivity.kt
+++ app/src/main/java/com/example/week05day01/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.week05day01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var textView:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectViews()
+    }
+
+    private fun connectViews() {
+        textView = findViewById(R.id.text_view_activity_main)
     }
 }
